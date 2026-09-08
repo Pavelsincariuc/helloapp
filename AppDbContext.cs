7cb572001054a892a1acb9789e50174c34424efc
guest <1guest@Pavels-MacBook-Air.local>
2026-09-08 09:59:26 +0300

    Add Order model, EF Core One-to-Many relation and seeding

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+namespace helloapp;
+
 public class AppDbContext : DbContext
 {
     public DbSet<User> Users => Set<User>();
+    public DbSet<Order> Orders => Set<Order>();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        
         optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=testdb;Username=postgres;Password=");
     }
 }
